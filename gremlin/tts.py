d8bc2defe28a83c982520bcded6f4335d122d01d
Lionel Ott <white.magic@gmx.de>
2015-04-06 14:00:19 +1000

    Text to speech module using Windows SAPI

---
+++ gremlin/tts.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8; -*-
+
+# Copyright (C) 2015 Lionel Ott
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+This module provides convenient access to the Microsoft SAPI text
+to speech system.
+"""
+
+import win32com.client
+
+from gremlin import util
+
+
+class TextToSpeech(object):
+
+    def __init__(self):
+        """Creates a new instance."""
+        self._speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+    def speak(self, text):
+        """Queues the given text to be spoken by SAPI using the async flag.
+
+        Since the text is queued asynchronously this method returns
+        immediately.
+
+        :param text the text to speak
+        """
+        self._speaker.Speak(text, 1)
+
+    def set_volume(self, value):
+        """Sets the volume anywhere between 0 and 100.
+
+        :param value the new volume value
+        """
+        self._speaker.Volume = int(util.clamp(value, 0, 100))
+
+    def set_rate(self, value):
+        """Sets the speaking speed between -10 and 10.
+
+        Negative values slow speech down while positive values speed
+        it up.
+
+        :param value the new speaking rate
+        """
+        self._speaker.Rate = int(util.clamp(value, -10, 10))
