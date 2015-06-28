9b9c1e0a20ef10a5ebef5fc926ae86735beab2ea
Lionel Ott <white.magic@gmx.de>
2015-06-28 21:32:46 +1000

    New UI input type enumeration added
    - infrastructure needed to handle hat and hat direction events properly
    - UI and XML profile now work with the UiInputType while the runtime system works with InputType
      which is system event dependent

---
+++ gremlin/common.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8; -*-
+
+# Copyright (C) 2015 Lionel Ott
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import enum
+
+
+class UiInputType(enum.Enum):
+
+    """Enumeration of possible UI input types."""
+
+    Keyboard = 1
+    JoystickAxis = 2
+    JoystickButton = 3
+    JoystickHat = 4
+    JoystickHatDirection = 5
+    Count = 6
+
+
+def ui_input_type_to_name(input_type):
+    lookup = {
+        UiInputType.Keyboard: "Keyboard",
+        UiInputType.JoystickAxis: "Axis",
+        UiInputType.JoystickButton: "Button",
+        UiInputType.JoystickHat: "Hat",
+        UiInputType.JoystickHatDirection: "Hat"
+    }
+    return lookup[input_type]
+
+
+def index_to_direction(direction):
+    lookup = {
+        1: "Up",
+        2: "Up & Right",
+        3: "Right",
+        4: "Down & Right",
+        5: "Down",
+        6: "Down & Left",
+        7: "Left",
+        8: "Up & Left"
+    }
+    return lookup[int(direction)]
