54e9193273ea2f9a54b7c830f1fcfc237bced91a
Lionel Ott <white.magic@gmx.de>
2016-04-05 22:28:52 +1000

    Turn config class into a singleton to prevent file corruption

--- gremlin/config.py
+++ gremlin/config.py
@@ -24,6 +24,7 @@ from PyQt5 import QtCore
 import gremlin.util
 
 
+@gremlin.util.SingletonDecorator
 class Configuration(object):
 
     """Responsible for loading and saving configuration data."""
