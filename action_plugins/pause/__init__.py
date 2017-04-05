6d0aa9d788b636b21052570b2c835caba49b2ffb
WhiteMagic <white.magic@gmx.de>
2017-04-05 23:35:18 +1000

    Update copyright year line

--- action_plugins/pause/__init__.py
+++ action_plugins/pause/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8; -*-
 
-# Copyright (C) 2015 - 2016 Lionel Ott
+# Copyright (C) 2015 - 2017 Lionel Ott
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
