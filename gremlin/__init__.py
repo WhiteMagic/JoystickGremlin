c9d12c6212a33694bc0ce4577ef7167689dcd4fa
WhiteMagic <white.magic@gmx.de>
2017-04-05 23:53:53 +1000

    Update module init file

--- gremlin/__init__.py
+++ gremlin/__init__.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8; -*-
+
+# Copyright (C) 2015 - 2017 Lionel Ott
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gremlin.action_containers
+import gremlin.base_classes
 import gremlin.code_generator
 import gremlin.code_runner
 import gremlin.common
@@ -8,14 +27,15 @@ import gremlin.error
 import gremlin.event_handler
 import gremlin.fsm
 import gremlin.input_devices
+import gremlin.joystick_handling
 import gremlin.keyboard_hook
 import gremlin.macro
 import gremlin.plugin_manager
 import gremlin.process_monitor
 import gremlin.profile
 import gremlin.profile_creator
+import gremlin.repeater
 import gremlin.shared_state
 import gremlin.spline
-import gremlin.template
 import gremlin.tts
 import gremlin.util
