8e34fed88242162340479d4fba53a41ed8959a79
WhiteMagic <white.magic@gmx.de>
2017-04-05 23:58:20 +1000

    Additional code reorganization

---
+++ gremlin/base_classes.py
@@ -0,0 +1,284 @@
+# -*- coding: utf-8; -*-
+
+# Copyright (C) 2015 - 2017 Lionel Ott
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
+#from gremlin.code_generator import template_helpers
+import gremlin
+from . import common, error, plugin_manager, profile
+
+
+class AbstractAction(profile.ProfileData):
+
+    """Base class for all actions that can be encoded via the XML and
+    UI system."""
+
+    def __init__(self, parent):
+        """Creates a new instance.
+
+        :parent the InputItem which is the parent to this action
+        """
+        assert isinstance(parent, AbstractContainer)
+        super().__init__(parent)
+        self.condition = self._create_condition()
+
+    def from_xml(self, node):
+        """Populates the instance with data from the given XML node.
+
+        :param node the XML node to populate fields with
+        """
+        super().from_xml(node)
+        self.condition.from_xml(node)
+
+    def to_xml(self):
+        """Returns a XML node representing the instance's contents.
+
+        :return XML node representing the state of this instance
+        """
+        node = super().to_xml()
+        self.condition.to_xml(node)
+        return node
+
+    def icon(self):
+        """Returns the icon to use when representing the action.
+
+        :return icon to use
+        """
+        raise error.MissingImplementationError(
+            "AbstractAction.icon not implemented in subclass"
+        )
+
+    def _create_condition(self):
+        input_type = self.get_input_type()
+        if input_type in [
+                common.InputType.JoystickButton,
+                common.InputType.Keyboard
+        ]:
+            return ButtonCondition()
+        elif input_type == common.InputType.JoystickAxis:
+            return AxisCondition()
+        elif input_type == common.InputType.JoystickHat:
+            return HatCondition()
+
+    def _code_generation(self, template_name, params):
+        """Generates the code using the provided data.
+
+        :param template_name base name of the templates
+        :param params the parameters to pass to the template
+        :return CodeBlock object containing the generated code
+        """
+        # Generate body and global code
+        # TODO: these ids no longer are part of AbstractAction but ProfileData
+        # params["axis_button_name"] = "axis_button_{:04d}".format(
+        #     AbstractAction.next_code_id
+        # )
+        # params["axis_button_cb"] = "axis_button_callback_{:04d}".format(
+        #     AbstractAction.next_code_id
+        # )
+        #params["helpers"] = template_helpers
+
+        # Insert additional common parameters
+        params["InputType"] = common.InputType
+        params["input_type"] = params["entry"].get_input_type()
+        params["id"] = profile.ProfileData.next_code_id
+        params["gremlin"] = gremlin
+
+        tpl_lookup = TemplateLookup(directories=["."])
+        body_code = Template(
+            filename="action_plugins/{}/body.tpl".format(template_name),
+            lookup=tpl_lookup
+        ).render(
+            **params
+        )
+        global_code = Template(
+            filename="action_plugins/{}/global.tpl".format(template_name),
+            lookup=tpl_lookup
+        ).render(
+            **params
+        )
+
+        # Put required conditions around the body block
+        body_code = self.condition.to_code(body_code)
+
+        return profile.CodeBlock(body_code, global_code)
+
+
+class AbstractContainer(profile.ProfileData):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.actions = []
+
+    def add_action(self, action):
+        assert isinstance(action, AbstractAction)
+        self.actions.append(action)
+
+    def is_valid(self):
+        state = self._is_valid()
+        for action in self.actions:
+            if action is None:
+                state = False
+            else:
+                state = state & action.is_valid()
+        return state
+
+    def from_xml(self, node):
+        super().from_xml(node)
+        self._parse_action_xml(node)
+
+    def _parse_action_xml(self, node):
+        action_name_map = plugin_manager.ActionPlugins().tag_map
+        for child in node:
+            if child.tag not in action_name_map:
+                logging.getLogger("system").warning(
+                    "Unknown node present: {}".format(child.tag)
+                )
+                continue
+            entry = action_name_map[child.tag](self)
+            entry.from_xml(child)
+            self.actions.append(entry)
+
+
+class AxisCondition:
+
+    """Indicates when an action associated with an axis is to be run."""
+
+    def __init__(self, is_active=False, lower_limit=0.0, upper_limit=0.0):
+        """Creates a new instance.
+
+        :param lower_limit lower axis limit of the activation range
+        :param upper_limit upper axis limit of the activation range
+        """
+        self.is_active = is_active
+        self.lower_limit = lower_limit
+        self.upper_limit = upper_limit
+
+    def from_xml(self, node):
+        self.is_active = profile.parse_bool(node.get("is-active"))
+        self.lower_limit = profile.parse_float(node.get("lower-limit"))
+        self.upper_limit = profile.parse_float(node.get("upper-limit"))
+
+    def to_xml(self, node):
+        node.set("is-active", str(self.is_active))
+        node.set("lower-limit", str(self.lower_limit))
+        node.set("upper-limit", str(self.upper_limit))
+
+    def to_code(self, code):
+        return code
+
+
+class ButtonCondition:
+
+    """Indicates when an action associated with a button is to be run"""
+
+    def __init__(self, on_press=True, on_release=False):
+        """Creates a new instance.
+
+        :param on_press when True the action is executed when the button
+            is pressed
+        :param on_release when True the action is execute when the
+            button is released
+        """
+        self.on_press = on_press
+        self.on_release = on_release
+
+    def from_xml(self, node):
+        try:
+            on_press = profile.parse_bool(node.get("on-press"))
+        except error.ProfileError:
+            on_press = None
+        try:
+            on_release = profile.parse_bool(node.get("on-release"))
+        except error.ProfileError:
+            on_release = None
+
+        # No valid data at all, set default values
+        if on_press is None and on_release is None:
+            self.on_press = True
+            self.on_release = False
+        # Some valid data handle it
+        else:
+            self.on_press = False if on_press is None else on_press
+            self.on_release = False if on_release is None else on_release
+
+    def to_xml(self, node):
+        node.set("on-press", str(self.on_press))
+        node.set("on-release", str(self.on_release))
+
+    def to_code(self, code):
+        tpl_lookup = TemplateLookup(directories=["."])
+        tpl = Template(
+            filename="templates/button_condition.tpl",
+            lookup=tpl_lookup
+        )
+        return tpl.render(
+            code=code,
+            on_press=self.on_press,
+            on_release=self.on_release
+        )
+
+
+class HatCondition:
+
+    """Indicates when an action associated with a hat is to be run."""
+
+    def __init__(
+            self,
+            on_n=False,
+            on_ne=False,
+            on_e=False,
+            on_se=False,
+            on_s=False,
+            on_sw=False,
+            on_w=False,
+            on_nw=False
+    ):
+        """Creates a new instance."""
+        self.on_n = on_n
+        self.on_ne = on_ne
+        self.on_e = on_e
+        self.on_se = on_se
+        self.on_s = on_s
+        self.on_sw = on_sw
+        self.on_w = on_w
+        self.on_nw = on_nw
+
+    def from_xml(self, node):
+        self.on_n = profile.read_bool(node, "on-n")
+        self.on_ne = profile.read_bool(node, "on-ne")
+        self.on_e = profile.read_bool(node, "on-e")
+        self.on_se = profile.read_bool(node, "on-se")
+        self.on_s = profile.read_bool(node, "on-s")
+        self.on_sw = profile.read_bool(node, "on-sw")
+        self.on_w = profile.read_bool(node, "on-w")
+        self.on_nw = profile.read_bool(node, "on-nw")
+
+    def to_xml(self, node):
+        print(self.on_n)
+        node.set("on-n", str(self.on_n))
+        node.set("on-ne", str(self.on_ne))
+        node.set("on-e", str(self.on_e))
+        node.set("on-se", str(self.on_se))
+        node.set("on-s", str(self.on_s))
+        node.set("on-sw", str(self.on_sw))
+        node.set("on-w", str(self.on_w))
+        node.set("on-nw", str(self.on_nw))
+
+    def to_code(self, code):
+        return code
---
+++ gremlin/joystick_handling.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8; -*-
+
+# Copyright (C) 2015 - 2017 Lionel Ott
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import logging
+import sdl2
+
+from . import error
+from vjoy import vjoy
+
+# List of all joystick devices
+_joystick_devices = []
+
+class VJoyProxy:
+
+    """Manages the usage of vJoy and allows shared access all callbacks."""
+
+    vjoy_devices = {}
+
+    def __getitem__(self, key):
+        """Returns the requested vJoy instance.
+
+        :param key id of the vjoy device
+        :return the corresponding vjoy device
+        """
+        if key in VJoyProxy.vjoy_devices:
+            return VJoyProxy.vjoy_devices[key]
+        else:
+            if not isinstance(key, int):
+                raise error.GremlinError("Integer ID for vjoy device ID expected")
+
+            device = vjoy.VJoy(key)
+            VJoyProxy.vjoy_devices[key] = device
+            return device
+
+    @classmethod
+    def reset(cls):
+        """Relinquishes control over all held VJoy devices."""
+        for device in VJoyProxy.vjoy_devices.values():
+            device.invalidate()
+        VJoyProxy.vjoy_devices = {}
+
+
+class JoystickDeviceData:
+
+    """Represents data about a joystick like input device."""
+
+    def __init__(self, device):
+        """Initializes the device data based on the given device.
+
+        :param device pyGame joystick object
+        """
+        self._hardware_id = get_device_guid(device)
+        self._windows_id = sdl2.SDL_JoystickInstanceID(device)
+        name_object = sdl2.SDL_JoystickName(device)
+        if name_object is None:
+            self._name = "Unknown device"
+            logging.getLogger("system").error(
+                "Encountered an invalid device name"
+            )
+        else:
+            self._name = name_object.decode("utf-8")
+        self._is_virtual = self._name == "vJoy Device"
+        self._axes = sdl2.SDL_JoystickNumAxes(device)
+        self._buttons = sdl2.SDL_JoystickNumButtons(device)
+        self._hats = sdl2.SDL_JoystickNumHats(device)
+        self._vjoy_id = 0
+
+    @property
+    def hardware_id(self):
+        return self._hardware_id
+
+    @property
+    def windows_id(self):
+        return self._windows_id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def is_virtual(self):
+        return self._is_virtual
+
+    @property
+    def axes(self):
+        return self._axes
+
+    @property
+    def buttons(self):
+        return self._buttons
+
+    @property
+    def hats(self):
+        return self._hats
+
+    @property
+    def vjoy_id(self):
+        return self._vjoy_id
+
+    def __hash__(self):
+        return hash((self.hardware_id, self.windows_id))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+
+def get_device_guid(device):
+    """Returns the GUID of the provided device.
+
+    :param device SDL2 joystick device for which to get the GUID
+    :return GUID for the provided device
+    """
+    vendor_id = sdl2.SDL_JoystickGetVendor(device)
+    product_id = sdl2.SDL_JoystickGetProduct(device)
+    return (vendor_id << 16) + product_id
+
+
+def joystick_devices():
+    """Returns the list of joystick like devices.
+
+    :return list containing information about all joystick like devices
+    """
+    global _joystick_devices
+
+    # Get all connected devices
+    devices = []
+    for i in range(sdl2.SDL_NumJoysticks()):
+        joy = sdl2.SDL_JoystickOpen(i)
+        if joy is None:
+            logging.getLogger("system").error(
+                "Invalid joystick device at id {}".format(i)
+            )
+        else:
+            devices.append(JoystickDeviceData(joy))
+
+    # Compare existing versus observed devices and only proceed if there
+    # is a difference
+    device_added = False
+    device_removed = False
+    for new_dev in devices:
+        if new_dev not in _joystick_devices:
+            device_added = True
+    for old_dev in _joystick_devices:
+        if old_dev not in devices:
+            device_removed = True
+
+    if not device_added and not device_removed:
+        return _joystick_devices
+
+    # Create hashes based on number of inputs for each virtual device. As we
+    # absolutely need to be able to assign the SDL device to the correct
+    # vJoy device we will not proceed if this mapping cannot be made without
+    # ambiguity.
+    vjoy_lookup = {}
+    for i, dev in enumerate(devices):
+        if not dev.is_virtual:
+            continue
+        hash_value = (dev.axes, dev.buttons, dev.hats)
+        if hash_value in vjoy_lookup:
+            raise error.GremlinError(
+                "Indistinguishable vJoy devices present"
+            )
+        vjoy_lookup[hash_value] = i
+
+    # For virtual joysticks query them id by id until we have found all active
+    # devices
+    vjoy_proxy = VJoyProxy()
+
+    # Try each possible vJoy device and if it exists find the matching device
+    # as detected by SDL
+    for i in range(1, 17):
+        try:
+            vjoy_dev = vjoy_proxy[i]
+            hash_value = (
+                # This is needed as we have two names for each axis
+                int(vjoy_dev.axis_count / 2),
+                vjoy_dev.button_count,
+                vjoy_dev.hat_count
+            )
+            if hash_value in vjoy_lookup:
+                devices[vjoy_lookup[hash_value]]._vjoy_id = vjoy_dev.vjoy_id
+
+            if hash_value not in vjoy_lookup:
+                raise error.GremlinError(
+                    "Unable to match vJoy device to windows device data"
+                )
+        except error.VJoyError:
+            pass
+
+    # Reset all devices so we don't hog the ones we aren't actually using
+    VJoyProxy.reset()
+
+    _joystick_devices = devices
+    return _joystick_devices
