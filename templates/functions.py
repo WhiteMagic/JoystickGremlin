c770dcd47fe0c474d17d208118fab8ddfa6685a2
WhiteMagic <white.magic@gmx.de>
2017-04-14 17:45:37 +1000

    Additional template helper functions added

--- templates/functions.py
+++ templates/functions.py
@@ -1,3 +1,6 @@
+import gremlin.base_classes
+
+
 def indent(context, content, spaces):
     text = ""
     lines = [line for line in content.splitlines() if len(line) > 0]
@@ -8,3 +11,19 @@ def indent(context, content, spaces):
         if len(lines) > 1:
             text += "{}{}".format(" " * spaces, lines[-1])
     return text
+
+
+def create_condition(context, condition):
+    if isinstance(condition, gremlin.base_classes.ButtonCondition):
+        return "gremlin.actions.ButtonCondition({}, {})".format(
+            condition.on_press,
+            condition.on_release
+        )
+    elif isinstance(condition, gremlin.base_classes.AxisCondition):
+        return "gremlin.actions.AxisCondition()"
+
+
+def coords_to_string(context, coordinates):
+    return "[{}]".format(", ".join(
+            ["({:.4f}, {:.4f})".format(e[0], e[1]) for e in coordinates])
+        )
