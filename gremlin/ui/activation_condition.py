63fcc1782240be0a93948da6243373566bcec997
WhiteMagic <white.magic@gmx.de>
2017-09-19 23:15:17 +1000

    UI updates to display activation condition stuff

---
+++ gremlin/ui/activation_condition.py
@@ -0,0 +1,351 @@
+# -*- coding: utf-8; -*-
+
+# Copyright (C) 2015 - 2017 Lionel Ott
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt5 import QtCore, QtWidgets
+
+import logging
+
+from gremlin import base_classes, input_devices, macro
+from gremlin.common import InputType
+from . import common
+
+
+class ActivationConditionWidget(QtWidgets.QGroupBox):
+
+    # Signal which is emitted whenever the widget's contents change
+    modified = QtCore.pyqtSignal()
+
+    # Maps activation type name to index
+    activation_type_to_index = {
+        None: 0,
+        "action": 1,
+        "container": 2
+    }
+
+    def __init__(self, profile_data, parent=None):
+        super().__init__(parent)
+        self.profile_data = profile_data
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.setTitle("Activation Condition")
+        self._create_ui()
+
+    def _create_ui(self):
+        self.granularity_selector = QtWidgets.QComboBox()
+        self.granularity_selector.addItem("None")
+        self.granularity_selector.addItem("Action")
+        self.granularity_selector.addItem("Container")
+        self.granularity_selector.setCurrentIndex(
+            ActivationConditionWidget.activation_type_to_index[
+                self.profile_data.activation_condition_type
+            ]
+        )
+        self.granularity_selector.currentIndexChanged.connect(
+            self._granularity_changed_cb
+        )
+
+        self.controls_layout = QtWidgets.QHBoxLayout()
+        self.controls_layout.addWidget(QtWidgets.QLabel("Condition on: "))
+        self.controls_layout.addWidget(self.granularity_selector)
+        self.controls_layout.addStretch()
+
+        self.main_layout.addLayout(self.controls_layout)
+
+        if self.profile_data.activation_condition_type == "container":
+            self.condition_model = ConditionModel(
+                self.profile_data.activation_condition
+            )
+            self.condition_view = ConditionView()
+            self.condition_view.set_model(self.condition_model)
+            self.condition_view.redraw()
+            self.main_layout.addWidget(self.condition_view)
+
+        self.main_layout.addStretch()
+
+    def _granularity_changed_cb(self, index):
+        index_to_type = {
+            0: None,
+            1: "action",
+            2: "container"
+        }
+        self.profile_data.activation_condition_type = index_to_type[index]
+
+        if self.profile_data.activation_condition_type == "container":
+            if self.profile_data.activation_condition is None:
+                self.profile_data.activation_condition = \
+                    base_classes.ActivationCondition()
+            else:
+                self.profile_data.activation_condition = None
+
+        self.modified.emit()
+
+
+class AbstractConditionWidget(QtWidgets.QWidget):
+
+    def __init__(self, condition_data, parent=None):
+        super().__init__(parent)
+        self.condition_data = condition_data
+
+        self.main_layout = QtWidgets.QHBoxLayout(self)
+        self._create_ui()
+
+    def _create_ui(self):
+        pass
+
+
+class KeyboardConditionWidget(AbstractConditionWidget):
+
+    def __init__(self, condition_data, parent=None):
+        super().__init__(condition_data, parent)
+
+    def _create_ui(self):
+        self.key_label = QtWidgets.QLabel("Not set")
+        if self.condition_data.scan_code is not None:
+            self.key_label.setText("<b>{}</b>".format(
+                macro.key_from_code(
+                    self.condition_data.scan_code,
+                    self.condition_data.extended
+                ).name
+            ))
+        self.record_button = QtWidgets.QPushButton("Change")
+        self.record_button.clicked.connect(self._request_user_input)
+
+        self.state_dropdown = QtWidgets.QComboBox()
+        self.state_dropdown.addItem("Pressed")
+        self.state_dropdown.addItem("Released")
+
+        self.main_layout.addWidget(QtWidgets.QLabel("Activate if"))
+        self.main_layout.addWidget(self.key_label)
+        self.main_layout.addWidget(QtWidgets.QLabel("is"))
+        self.main_layout.addWidget(self.state_dropdown)
+        self.main_layout.addWidget(self.record_button)
+
+    def _key_pressed_cb(self, key):
+        self.condition_data.scan_code = key.identifier[0]
+        self.condition_data.extended = key.identifier[1]
+        self.key_label.setText("<b>{}</b>".format(
+            macro.key_from_code(
+                self.condition_data.scan_code,
+                self.condition_data.extended
+            ).name
+        ))
+
+    def _request_user_input(self):
+        """Prompts the user for the input to bind to this item."""
+        self.button_press_dialog = common.InputListenerWidget(
+            self._key_pressed_cb,
+            [InputType.Keyboard],
+            return_kb_event=True,
+            multi_keys=False
+        )
+
+        # Display the dialog centered in the middle of the UI
+        root = self
+        while root.parent():
+            root = root.parent()
+        geom = root.geometry()
+
+        self.button_press_dialog.setGeometry(
+            geom.x() + geom.width() / 2 - 150,
+            geom.y() + geom.height() / 2 - 75,
+            300,
+            150
+        )
+        self.button_press_dialog.show()
+
+
+class JoystickConditionWidget(AbstractConditionWidget):
+
+    def __init__(self, condition_data, parent=None):
+        super().__init__(condition_data, parent)
+        self.input_event = None
+        self._name = ""
+
+    def _create_ui(self):
+        common.clear_layout(self.main_layout)
+
+        self.record_button = QtWidgets.QPushButton("Change")
+        self.record_button.clicked.connect(self._request_user_input)
+
+        if self.condition_data.input_type == InputType.JoystickAxis:
+            self._axis_ui()
+        elif self.condition_data.input_type == InputType.JoystickButton:
+            self._button_ui()
+        elif self.condition_data.input_type == InputType.JoystickHat:
+            self._hat_ui()
+        self.main_layout.addWidget(self.record_button)
+
+    def _axis_ui(self):
+        self.lower = common.DynamicDoubleSpinBox()
+        self.lower.setMinimum(-1.0)
+        self.lower.setMaximum(1.0)
+        self.lower.setSingleStep(0.05)
+        self.lower.setDecimals(4)
+        self.upper = common.DynamicDoubleSpinBox()
+        self.upper.setMinimum(-1.0)
+        self.upper.setMaximum(1.0)
+        self.upper.setDecimals(4)
+        self.upper.setSingleStep(0.05)
+
+        self.state_dropdown = QtWidgets.QComboBox()
+        self.state_dropdown.addItem("Inside")
+        self.state_dropdown.addItem("Outside")
+
+        self.main_layout.addWidget(QtWidgets.QLabel(
+            "Activate if {} Axis {:d} is".format(
+                self._name,
+                self.condition_data.input_id
+            )
+        ))
+        self.main_layout.addWidget(self.state_dropdown)
+        self.main_layout.addWidget(self.lower)
+        self.main_layout.addWidget(QtWidgets.QLabel("and"))
+        self.main_layout.addWidget(self.upper)
+
+    def _button_ui(self):
+        self.state_dropdown = QtWidgets.QComboBox()
+        self.state_dropdown.addItem("Pressed")
+        self.state_dropdown.addItem("Released")
+
+        self.main_layout.addWidget(QtWidgets.QLabel(
+            "Activate if {} Button {:d} is".format(
+                self._name,
+                self.condition_data.input_id
+            )
+        ))
+        self.main_layout.addWidget(self.state_dropdown)
+
+    def _hat_ui(self):
+        pass
+
+    def _input_pressed_cb(self, event):
+        self.condition_data.device_id = event.hardware_id
+        self.condition_data.windows_id = event.windows_id
+        self.condition_data.input_type = event.event_type
+        self.condition_data.input_id = event.identifier
+
+        self._name = input_devices.JoystickProxy()[event.windows_id].name
+        self._create_ui()
+
+    def _request_user_input(self):
+        """Prompts the user for the input to bind to this item."""
+        self.input_dialog = common.InputListenerWidget(
+            self._input_pressed_cb,
+            [
+                InputType.JoystickAxis,
+                InputType.JoystickButton,
+                InputType.JoystickHat
+            ],
+            return_kb_event=False,
+            multi_keys=False
+        )
+
+        # Display the dialog centered in the middle of the UI
+        root = self
+        while root.parent():
+            root = root.parent()
+        geom = root.geometry()
+
+        self.input_dialog.setGeometry(
+            geom.x() + geom.width() / 2 - 150,
+            geom.y() + geom.height() / 2 - 75,
+            300,
+            150
+        )
+        self.input_dialog.show()
+
+
+class PressReleaseConditionWidget(AbstractConditionWidget):
+
+    def __init__(self, condition_data, parent=None):
+        super().__init__(condition_data, parent)
+
+    def _create_ui(self):
+        pass
+
+
+class ConditionModel(common.AbstractModel):
+
+    def __init__(self, condition_data, parent=None):
+        super().__init__(parent)
+        self.condition_data = condition_data
+
+    def rows(self):
+        return len(self.condition_data.conditions)
+
+    def data(self, index):
+        return self.condition_data.conditions[index]
+
+    def add_condition(self, condition_data):
+        self.condition_data.conditions.append(condition_data)
+        self.data_changed.emit()
+
+
+class ConditionView(common.AbstractView):
+
+    condition_map = {
+        "Keyboard": [base_classes.KeyboardCondition,
+                     KeyboardConditionWidget],
+        "Joystick": [base_classes.JoystickCondition,
+                     JoystickConditionWidget]
+    }
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.controls_layout = QtWidgets.QHBoxLayout()
+        self.conditions_layout = QtWidgets.QVBoxLayout()
+
+        self.main_layout.addLayout(self.controls_layout)
+        self.main_layout.addLayout(self.conditions_layout)
+
+        self.rule_selector = QtWidgets.QComboBox()
+        self.rule_selector.addItem("All")
+        self.rule_selector.addItem("Any")
+        self.controls_layout.addWidget(QtWidgets.QLabel("Execute if"))
+        self.controls_layout.addWidget(self.rule_selector)
+        self.controls_layout.addWidget(QtWidgets.QLabel("condition(s) are met"))
+
+        self.condition_selector = QtWidgets.QComboBox()
+        self.condition_selector.addItem("Keyboard")
+        self.condition_selector.addItem("Joystick")
+        self.condition_add_button = QtWidgets.QPushButton("Add")
+        self.condition_add_button.clicked.connect(self._add_condition)
+        self.controls_layout.addWidget(QtWidgets.QLabel("Add condition"))
+        self.controls_layout.addWidget(self.condition_selector)
+        self.controls_layout.addWidget(self.condition_add_button)
+
+    def redraw(self):
+        common.clear_layout(self.conditions_layout)
+
+        lookup = {}
+        for entry in ConditionView.condition_map.values():
+            lookup[entry[0]] = entry[1]
+
+        for i in range(self.model.rows()):
+            data = self.model.data(i)
+            self.conditions_layout.addWidget(
+                lookup[type(data)](data)
+            )
+
+    def _add_condition(self):
+        data_type = ConditionView.condition_map[
+            self.condition_selector.currentText()
+        ][0]
+        self.model.add_condition(data_type())
+
+
