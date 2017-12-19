e57565eaa3638afbc1baac2a435dde213cbe0ca8
WhiteMagic <white.magic@gmx.de>
2017-12-19 13:01:26 +0100

    Button release code now works with keyboard inputs too

--- gremlin/input_devices.py
+++ gremlin/input_devices.py
@@ -511,7 +511,11 @@ class ButtonReleaseActions(QtCore.QObject):
 
         self._registry = {}
         el = event_handler.EventListener()
-        el.joystick_event.connect(self._joystick_cb)
+        el.joystick_event.connect(self._input_event_cb)
+        el.keyboard_event.connect(self._input_event_cb)
+        eh = event_handler.EventHandler()
+        self._current_mode = eh.active_mode
+        eh.mode_changed.connect(self._mode_changed_cb)
 
     def register_callback(self, callback, physical_event):
         """Registers a callback with the system.
@@ -525,9 +529,7 @@ class ButtonReleaseActions(QtCore.QObject):
 
         if release_evt not in self._registry:
             self._registry[release_evt] = []
-        self._registry[release_evt].append(
-            callback
-        )
+        self._registry[release_evt].append((callback, self._current_mode))
 
     def register_button_release(self, vjoy_input, physical_event):
         """Registers a physical and vjoy button pair for tracking.
@@ -558,18 +560,23 @@ class ButtonReleaseActions(QtCore.QObject):
         if vjoy[vjoy_input[0]].is_button_valid(vjoy_input[1]):
             vjoy[vjoy_input[0]].button(vjoy_input[1]).is_pressed = False
 
-    def _joystick_cb(self, evt):
-        """Releases vjoy buttons if appropriate.
+    def _input_event_cb(self, evt):
+        """Runs callbacks associated with the given event.
 
-        If any vjoy buttons are associated with the event they are
-        released
-
-        :param evt the joystick event to process
+        :param evt the event to process
         """
         if evt in self._registry and not evt.is_pressed:
-            for callback in self._registry[evt]:
-                callback()
+            for entry in self._registry[evt]:
+                if entry[1] != self._current_mode:
+                    entry[0]()
             self._registry[evt] = []
+
+    def _mode_changed_cb(self, mode):
+        """Updates the current mode variable.
+
+        :param mode the new mode
+        """
+        self._current_mode = mode
 
 
 @common.SingletonDecorator
