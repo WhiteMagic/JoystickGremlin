2d28d3b9cde7de8176aedbbd9006af462691f834
WhiteMagic <white.magic@gmx.de>
2017-12-23 14:43:06 +0100

    HG control ui added

--- gremlin/ui/ui_gremlin.py
+++ gremlin/ui/ui_gremlin.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_gremlin.ui'
 #
-# Created by: PyQt5 UI code generator 5.5.1
+# Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
 
