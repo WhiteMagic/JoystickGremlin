923ce97697c0b62894521d17641cb4f8999258ba
WhiteMagic <white.magic@gmx.de>
2018-10-21 18:22:39 +1100

    Rework joystick initialization code

--- gremlin/joystick_handling.py
+++ gremlin/joystick_handling.py
@@ -17,15 +17,21 @@
 
 
 import logging
+import threading
 import time
 
 import sdl2
 
-from . import error, util
+from . import common, error, util
 from vjoy import vjoy
+
 
 # List of all joystick devices
 _joystick_devices = []
+
+# Joystick initializataion lock
+_joystick_init_lock = threading.Lock()
+
 
 
 class VJoyProxy:
@@ -215,102 +221,135 @@ def joystick_devices_initialization():
     Amongst other things this also ensures that each vJoy device has a correct
     windows id assigned to it.
     """
-    global _joystick_devices
+    global _joystick_devices, _joystick_init_lock
+
+    _joystick_init_lock.acquire()
+
     syslog = logging.getLogger("system")
+    syslog.debug("{:d} joysticks detected".format(sdl2.SDL_NumJoysticks()))
 
-    syslog.debug("{:d} joysticks found".format(sdl2.SDL_NumJoysticks()))
+    # Register all devices with the device registry to handle duplicate and
+    # non duplicate devices transparently.
+    devreg = common.DeviceRegistry()
+    devreg.reset()
 
-    # Get all connected devices
-    devices = []
+    # Process all connected devices in order to properly initialize the
+    # device registry
     for i in range(sdl2.SDL_NumJoysticks()):
         joy = sdl2.SDL_JoystickOpen(i)
         if joy is None:
             syslog.error("Invalid joystick device at id {}".format(i))
         else:
+            devreg.register(
+                get_device_guid(joy),
+                sdl2.SDL_JoystickInstanceID(joy)
+            )
+
+
+    # Process all devices again to detect those that have been added and those
+    # that have been removed since the last time this function ran.
+
+    # Accumulate all devices
+    devices = []
+    for i in range(sdl2.SDL_NumJoysticks()):
+        joy = sdl2.SDL_JoystickOpen(i)
+        if joy is not None:
             devices.append(JoystickDeviceData(joy))
 
     # Compare existing versus observed devices and only proceed if there
-    # is a difference
+    # is a change to avoid unneccessary work.
     device_added = False
     device_removed = False
     for new_dev in devices:
         if new_dev not in _joystick_devices:
             device_added = True
-            syslog.debug("Added: name={} windows_id={:d}".format(
+            syslog.debug("Added: name={} windows_id={:d} hardware_id={:d}".format(
                 new_dev.name,
-                new_dev.windows_id
+                new_dev.windows_id,
+                new_dev.hardware_id
             ))
     for old_dev in _joystick_devices:
         if old_dev not in devices:
             device_removed = True
-            syslog.debug("Removed: name={} windows_id={:d}".format(
+            syslog.debug("Removed: name={} windows_id={:d} hardware_id={:d}".format(
                 old_dev.name,
-                old_dev.windows_id
+                old_dev.windows_id,
+                old_dev.hardware_id
             ))
 
+    # Terminate if no change occurred
     if not device_added and not device_removed:
-        return _joystick_devices
+        _joystick_init_lock.release()
+        return
 
-    # Create hashes based on number of inputs for each virtual device. As we
-    # absolutely need to be able to assign the SDL device to the correct
-    # vJoy device we will not proceed if this mapping cannot be made without
-    # ambiguity.
+
+    # In order to associate vJoy devices and their ids correctly with SDL
+    # device ids a hash is constructed from the number of axes, buttons, and
+    # hats. This information is used to attempt to find unambiguous mappings
+    # between vJoy and SDL devices. If this is not possible Gremlin will
+    # terminate as this is a non-recoverable error.
+
     vjoy_lookup = {}
-    for i, dev in enumerate(devices):
-        if not dev.is_virtual:
-            continue
+    for dev in [dev for dev in devices if dev.is_virtual]:
         hash_value = (dev.axis_count, dev.buttons, dev.hats)
-        syslog.debug("vjoy windows id {:d}: {}".format(dev.windows_id, hash_value))
+        syslog.debug(
+            "vJoy windows id {:d}: {}".format(dev.windows_id, hash_value)
+        )
+
+        # Only unique combinations of axes, buttons, and hats are allowed
+        # for vJoy devices
         if hash_value in vjoy_lookup:
             raise error.GremlinError(
                 "Indistinguishable vJoy devices present.\n\n"
                 "vJoy devices have to differ in the number of "
-                "(at least one of) axis, buttons, or hats in order to work "
+                "(at least one of) axes, buttons, or hats in order to work "
                 "properly with Joystick Gremlin."
             )
-        vjoy_lookup[hash_value] = i
 
-    # For virtual joysticks query them id by id until we have found all active
-    # devices
+        vjoy_lookup[hash_value] = dev
+
+    # Query all vJoy devices in sequence until all have been processed and
+    # their matching SDL counterparts have been found.
     vjoy_proxy = VJoyProxy()
-
-    # Try each possible vJoy device and if it exists find the matching device
-    # as detected by SDL
     should_terminate = False
     for i in range(1, 17):
-        if vjoy.device_exists(i):
-            hash_value = (
-                vjoy.axis_count(i),
-                vjoy.button_count(i),
-                vjoy.hat_count(i)
-            )
-            devices[vjoy_lookup[hash_value]].set_vjoy_id(i)
-            syslog.debug("vJoy id {:d} exists".format(i))
-        else:
+        # Only process devices that actually exist
+        if not vjoy.device_exists(i):
             continue
 
-        if not vjoy.device_available(i):
-            syslog.debug("vJoy id {:} can't be acquired".format(i))
-            continue
-
-        vjoy_dev = vjoy_proxy[i]
+        # Compute a hash for the vJoy device and match it against the SDL
+        # device hashes
         hash_value = (
-            vjoy_dev.axis_count,
-            vjoy_dev.button_count,
-            vjoy_dev.hat_count
+            vjoy.axis_count(i),
+            vjoy.button_count(i),
+            vjoy.hat_count(i)
         )
-        if hash_value in vjoy_lookup:
-            # Set vJoy id and correctly setup the axis id mapping
-            # devices[vjoy_lookup[hash_value]]._vjoy_id = vjoy_dev.vjoy_id
-            axis_mapping = []
-            for j in range(vjoy_dev.axis_count):
-                axis_mapping.append((j+1, vjoy_dev.axis_id(j+1)))
-            devices[vjoy_lookup[hash_value]].set_axis_mapping(axis_mapping)
-            syslog.debug("vjoy id {:d}: {} - MATCH".format(i, hash_value))
 
-        if hash_value not in vjoy_lookup:
-            syslog.debug("vjoy id {:d}: {} - ERROR".format(i, hash_value))
+        # As we are ensured that no duplicate vJoy devices exist from
+        # the previous step we can directly link the SDL and vJoy device
+        if hash_value in vjoy_lookup:
+            vjoy_lookup[hash_value].set_vjoy_id(i)
+            syslog.debug("vjoy id {:d}: {} - MATCH".format(i, hash_value))
+        else:
             should_terminate = True
+            syslog.debug(
+                "vjoy id {:d}: {} - ERROR - vJoy device exists "
+                "SDL is missing".format(i, hash_value)
+            )
+
+        # If the device can be acquired, configure the mapping from
+        # vJoy axis id, which may not be sequential, to the
+        # sequential SDL axis id
+        if hash_value in vjoy_lookup:
+            try:
+                vjoy_dev = vjoy_proxy[i]
+
+                axis_mapping = []
+                for j in range(vjoy_dev.axis_count):
+                    axis_mapping.append((j + 1, vjoy_dev.axis_id(j + 1)))
+                vjoy_lookup[hash_value].set_axis_mapping(axis_mapping)
+            except error.VJoyError as e:
+                syslog.debug("vJoy id {:} can't be acquired".format(i))
 
     if should_terminate:
         raise error.GremlinError(
@@ -320,5 +359,8 @@ def joystick_devices_initialization():
     # Reset all devices so we don't hog the ones we aren't actually using
     vjoy_proxy.reset()
 
+    # Update device list which will be used when queries for joystick devices
+    # are made
     _joystick_devices = devices
-    return _joystick_devices
+
+    _joystick_init_lock.release()
