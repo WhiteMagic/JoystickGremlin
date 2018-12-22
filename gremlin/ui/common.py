1fe7bb06aa3bd793e0534d9004fdcf2fdaad3f0a
WhiteMagic <white.magic@gmx.de>
2018-12-22 18:33:59 +0100

    Fix inability to add negative sign in spin boxes

--- gremlin/ui/common.py
+++ gremlin/ui/common.py
@@ -193,7 +193,7 @@ class DynamicDoubleSpinBox(QtWidgets.QDoubleSpinBox):
 
     """Implements a double spin box which dynamically overwrites entries."""
 
-    valid_chars = [str(v) for v in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
+    valid_chars = [str(v) for v in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]] + ["-"]
     decimal_point = "."
 
     def __init__(self, parent=None):
