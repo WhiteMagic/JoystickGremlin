3dc5e9dfb5887728f85cc4fdec02cdca4333d8eb
Tahvohck <Tahvohck@gmail.com>
2019-09-17 20:26:10 -0400

    simplify API call generation, pull port out to int

--- gremlin/hid_cerberus.py
+++ gremlin/hid_cerberus.py
@@ -10,13 +10,13 @@ def _get_web(url):
 
 
 class HIDCerberus:
-    cerberus_URL = "http://localhost:26762/"
-    api_base = "api/v1/hidguardian/"
+    cerberus_API_URL = "http://localhost:/{port}/api/v1/hidguardian/"
+    cerberus_API_PORT = 26762
     api_purge_whitelist = "whitelist/purge"
     api_purge_devices = "affected/purge"
     api_whitelist_get = "whitelist/get"
-    api_whitelist_add = "whitelist/add/{}"
-    api_whitelist_rem = "whitelist/remove/{}"
+    api_whitelist_add = "whitelist/add/{pid}"
+    api_whitelist_rem = "whitelist/remove/{pid}"
     api_devices_get = "affected/get"
     api_devices_add = "affected/add"
     api_devices_rem = "affected/remove"
@@ -34,24 +34,29 @@ class HIDCerberus:
     def add_process(self, process_id):
         '''Requests that HID Cerberus add the PID to its whitelist
         :param process_id PID of the process to be added'''
-        resp = _get_web(
-            self.cerberus_URL + self.api_base +
-            self.api_whitelist_add.format(process_id)
+        API_CALL = (self.cerberus_API_URL + self.api_whitelist_add).format(
+            port = self.cerberus_API_PORT,
+            pid = process_id
         )
+        resp = _get_web(API_CALL)
         # TODO: Do some processing on the response? gremlin.util.log() maybe?
 
     def remove_process(self, process_id):
         '''Requests that HID Cerberus remove the PID from its whitelist
         :param process_id id of the process to be removed'''
-        resp = _get_web(
-            self.cerberus_URL + self.api_base +
-            self.api_whitelist_rem.format(process_id)
+        API_CALL = (self.cerberus_API_URL + self.api_whitelist_rem).format(
+            port = self.cerberus_API_PORT,
+            pid = process_id
         )
+        resp = _get_web(API_CALL)
         # TODO: Do some processing on the response? gremlin.util.log() maybe?
 
     def clear_process_list(self):
         '''Request HID Cerberus purge its PID whitelist'''
-        _get_web(self.cerberus_URL + self.api_base + self.api_purge_whitelist)
+        API_CALL = (self.cerberus_API_URL + self.api_purge_whitelist).format(
+            port = self.cerberus_API_PORT
+        )
+        resp = _get_web(API_CALL)
 
     def _create_device_string(self, vendor_id, product_id): pass
 
