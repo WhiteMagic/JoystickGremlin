31fdc19726b87641ae5e993ffa8731895a2ca3a7
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2020-11-20 00:34:30 -0800

    Iterate keys macro in reverse order

--- action_plugins/map_to_keyboard/__init__.py
+++ action_plugins/map_to_keyboard/__init__.py
@@ -105,7 +105,8 @@ class MapToKeyboardFunctor(AbstractFunctor):
             self.press.press(gremlin.macro.key_from_code(key[0], key[1]))
 
         self.release = gremlin.macro.Macro()
-        for key in action.keys:
+        # Execute release in reverse order
+        for key in reversed(action.keys):
             self.release.release(gremlin.macro.key_from_code(key[0], key[1]))
 
     def process_event(self, event, value):
