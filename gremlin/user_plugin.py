37ba7760fe7ba86e1edc225c3496c027fabde867
Moondragon85 <42060641+Moondragon85@users.noreply.github.com>
2021-09-02 09:42:34 +0200

    Correctly set value range on FloatVariables and increase decimals shown

--- gremlin/user_plugin.py
+++ gremlin/user_plugin.py
@@ -342,6 +342,8 @@ class NumericalVariable(AbstractVariable):
             )
         elif self.variable_type == common.PluginVariableType.Float:
             value_widget = QtWidgets.QDoubleSpinBox()
+            value_widget.setDecimals(3)
+            value_widget.setRange(self.min_value, self.max_value)
             value_widget.setValue(float(value))
             value_widget.valueChanged.connect(
                 lambda x: self.value_changed.emit({"value": x})
@@ -413,7 +415,7 @@ class FloatVariable(NumericalVariable):
             is_optional
         )
 
-        _init_numerical(self, 0, -1.0, 1.0)
+        _init_numerical(self, 0.0, -1.0, 1.0)
         self._load_from_registry(self._get_identifier())
 
 
