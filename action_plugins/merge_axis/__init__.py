e823a04e4ec0b1525714e04eddbbf8ac928cd6ba
WhiteMagic <white.magic@gmx.de>
2023-04-10 10:30:17 +0200

    Initial merge axis action work

--- action_plugins/merge_axis/__init__.py
+++ action_plugins/merge_axis/__init__.py
@@ -18,24 +18,72 @@
 
 from __future__ import annotations
 
-import copy
-import logging
-import threading
-import time
-from typing import List, Optional
+from enum import Enum
+from typing import List, NamedTuple, Optional
+import uuid
 from xml.etree import ElementTree
 
 from PySide6 import QtCore
 from PySide6.QtCore import Property, Signal, Slot
 
-from gremlin import error, event_handler, plugin_manager, \
-    profile_library, util
+import dill
+
+from gremlin import util
 from gremlin.base_classes import AbstractActionData, AbstractFunctor, Value
 from gremlin.config import Configuration
-from gremlin.tree import TreeNode
+from gremlin.error import GremlinError
+from gremlin.event_handler import Event
+from gremlin.profile import InputItemBinding, Library
 from gremlin.types import InputType, PropertyType
 
 from gremlin.ui.action_model import ActionModel
+
+
+class AxisIdentifier(NamedTuple):
+
+    """Identifies a single axis that is used in an axis merge."""
+
+    guid: uuid.UUID4 = dill.GUID_Invalid
+    axis_id: int = -1
+
+
+class MergeOperation(Enum):
+
+    """Represents the available merge operations."""
+
+    Average = 0
+    Minimum = 1
+    Maximum = 2
+
+    __enum_to_string = {
+        Average: "average",
+        Minimum: "minimum",
+        Maximum: "maximum"
+    }
+
+    __string_to_enum = {
+        "average": Average,
+        "minimum": Minimum,
+        "maximum": Maximum
+    }
+
+    @staticmethod
+    def to_string(value: MergeOperation) -> str:
+        try:
+            return MergeOperation.__enum_to_string[value]
+        except KeyError:
+            raise GremlinError(
+                "MergeOperation: invalid value in lookup '{value}'"
+            )
+    
+    @staticmethod
+    def to_enum(value: str) -> MergeOperation:
+        try:
+            return MergeOperation.__string_to_enum[value]
+        except KeyError:
+            raise GremlinError(
+                "MergeOperation: invalid value in lookup '{value}'"
+            )
 
 
 class MergeAxisFunctor(AbstractFunctor):
@@ -43,7 +91,7 @@ class MergeAxisFunctor(AbstractFunctor):
     def __init__(self, action: MergeAxisModel):
         super().__init__(action)
 
-    def process_event(self, event: event_handler.Event, value: Value) -> None:
+    def process_event(self, event: Event, value: Value) -> None:
         return super().process_event(event, value)
 
 
@@ -51,12 +99,11 @@ class MergeAxisModel(ActionModel):
 
     def __init__(
             self,
-            action_tree: profile_library.ActionTree,
-            input_type: InputType = InputType.JoystickButton,
-            parent: Optional[QtCore.QObject] = None
+            data: AbstractActionData,
+            binding: InputItemBinding,
+            parent: QtCore.QObject
     ):
-        super().__init__(action_tree, input_type, parent)
-
+        super().__init__(data, binding, parent)
 
     def _qml_path_impl(self) -> str:
         return "file:///" + QtCore.QFile(
@@ -79,20 +126,65 @@ class MergeAxisData(AbstractActionData):
 
     def __init__(
             self,
-            action_tree: profile_library.ActionTree,
-            input_type: InputType = InputType.JoystickAxis,
-            parent: Optional[QtCore.QObject] = None
+            behavior_type: InputType=InputType.JoystickButton
     ):
-        super().__init__(action_tree, input_type, parent)
+        super().__init__(behavior_type)
+
+        # Merge action information
+        self.axis_1 = AxisIdentifier()
+        self.axis_2 = AxisIdentifier()
+        self.operation = MergeOperation.Average
 
     def from_xml(self, node: ElementTree.Element) -> None:
-        return super().from_xml(node)
+        self._id = util.read_action_id(node)
+        self.axis_1.guid = util.read_property(
+            node, "axis1-guid", PropertyType.GUID
+        )
+        self.axis_1.axis_id = util.read_property(
+            node, "axis1-axis", PropertyType.Int
+        )
+        self.axis_2.guid = util.read_property(
+            node, "axis2-guid", PropertyType.GUID
+        )
+        self.axis_2.axis_id = util.read_property(
+            node, "axis2-axis", PropertyType.Int
+        )
+        self.operation = MergeOperation.to_enum(util.read_property(
+            node, "operation", PropertyType.String
+        ))
 
     def to_xml(self) -> ElementTree.Element:
-        return super().to_xml()
+        node = util.create_action_node(MergeAxisData.tag, self._id)
+        entries = [
+            ["axis1-guid", self.axis_1.guid, PropertyType.GUID],
+            ["axis1-axis", self.axis_1.axis_id, PropertyType.Int],
+            ["axis2-guid", self.axis_2.guid, PropertyType.GUID],
+            ["axis2-axis", self.axis_2.axis_id, PropertyType.Int],
+            [
+                "operation",
+                MergeOperation.to_string(self.operation),
+                PropertyType.String
+            ],
+        ]
+        util.append_property_nodes(node, entries)
+
+        return node
 
     def is_valid(self) -> bool:
         return super().is_valid()
+    
+    def _valid_selectors(self) -> List[str]:
+        return []
+
+    def _get_container(self, selector: str) -> List[AbstractActionData]:
+        raise GremlinError(f"{self.name}: has no containers")
+
+    def _handle_behavior_change(
+        self,
+        old_behavior: InputType,
+        new_behavior: InputType
+    ) -> None:
+        pass
 
 
 create = MergeAxisData
