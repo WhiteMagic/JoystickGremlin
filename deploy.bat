cec4a750e73b0bd4b5dac69ad7593c528dc7c8bc
code-monet <92645652+code-monet@users.noreply.github.com>
2026-03-27 10:47:53 -0700

    Limit pyside to a version where the input viewer still works. Also update deploy.bat to use the same commands as in nightly_build.yml

--- deploy.bat
+++ deploy.bat
@@ -1,21 +1,15 @@
 @echo "Starting to build Gremlin ..."
 cd /d %0\..
 
+@echo "Installing dependencies with Poetry ..."
+poetry install
+
 @echo "Building executable ..."
-pyinstaller -y --clean joystick_gremlin.spec
+poetry run pyinstaller -y --clean joystick_gremlin.spec
 
-@echo "Generating WIX ..."
-python generate_wix.py
-copy /Y joystick_gremlin.wxs dist\joystick_gremlin.wxs
-
-@echo "Building MSI installer ..."
+@echo "Compress the build ..."
 cd dist
-del /Q PFiles
-del joystick_gremlin.wixobj
-del joystick_gremlin.wixpdb
-del joystick_gremlin.msi
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" joystick_gremlin.wxs
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" -ext WixUiExtension joystick_gremlin.wixobj
+powershell -Command "Compress-Archive -Path * -DestinationPath joystick_gremlin_build.zip"
 
 cd ..
 
